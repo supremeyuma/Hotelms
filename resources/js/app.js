1636c072d4f332432e987c615308c2e1d553f87b
unknown <yumakparobo@gmail.com>
2025-12-10 02:19:39 +0100

    FIX - IMPORTED "Link" and "usePage" from intertiajs/vue
    This allows all links to be clickable
    Allows usepage to be called globally

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import '../css/app.css';
 import './bootstrap';
 
-import { createInertiaApp } from '@inertiajs/vue3';
+import { createInertiaApp, Link, usePage } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
