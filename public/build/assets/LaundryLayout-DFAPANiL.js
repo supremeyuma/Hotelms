5fe21c26ddfdebe2c5f761a647e8884690d3aa5a
unknown <yumakparobo@gmail.com>
2026-01-21 09:59:44 +0100

    Built npm files

--- public/build/assets/LaundryLayout-DFAPANiL.js
+++
@@ -1,1 +0,0 @@
-import{B as t}from"./BaseStaffLayout-B4Kl57lg.js";import{c as o,o as e,w as r,j as s}from"./app-DdNzErec.js";const l={__name:"LaundryLayout",setup(n){return(a,u)=>(e(),o(t,null,{default:r(()=>[s(a.$slots,"default")]),_:3}))}};export{l as _};
