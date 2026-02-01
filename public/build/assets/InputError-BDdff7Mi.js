053d091b2111a3e0b0f9def61aa8e5323bd9b3e3
unknown <yumakparobo@gmail.com>
2026-02-01 11:05:34 +0100

    built npm assests

---
+++ public/build/assets/InputError-BDdff7Mi.js
@@ -0,0 +1,1 @@
+import{j as s,H as t,d as a,b as r,t as o,o as c}from"./app-DxJBQBBB.js";const n={class:"text-sm text-red-600"},l={__name:"InputError",props:{message:{type:String}},setup(e){return(i,m)=>s((c(),a("div",null,[r("p",n,o(e.message),1)],512)),[[t,e.message]])}};export{l as _};
