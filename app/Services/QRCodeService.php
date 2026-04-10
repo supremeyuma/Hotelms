ea7436b32a4fbb7c3c966346fbbd88828f1c95fd
CodexSandboxOffline <yumakparobo@gmail.com>
2026-04-10 23:51:59 +0100

    fix: switch qr service rendering to svg

--- app/Services/QRCodeService.php
+++ app/Services/QRCodeService.php
@@ -6,9 +6,9 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class QrCodeService
 {
-    public function generatePng(string $content, int $size = 400): string
+    public function generateSvg(string $content, int $size = 400): string
     {
-        return QrCode::format('png')
+        return QrCode::format('svg')
             ->size($size)
             ->margin(2)
             ->errorCorrection('H')
